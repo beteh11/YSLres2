3ff53717f92d864ba647be9152f41361d7cc036f
beteh11 <beteh_11@hotmail.com>
2024-08-15 22:07:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const printReceipt = require('./print');
 const app = express();
 
 app.use(express.json());
+
+function printReceipt(data) {
+  const device = new escpos.Network(process.env.PRINTER_IP);
+  const printer = new escpos.Printer(device);
+
+  device.open(function() {
+    printer
+      .text(data)
+      .cut()
+      .close();
+  });
+}
 
 app.post('/print', (req, res) => {
   const { data } = req.body;
@@ -10,6 +21,7 @@ app.post('/print', (req, res) => {
   res.send('Print job sent');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000; // Use PORT from environment variables or default to 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
